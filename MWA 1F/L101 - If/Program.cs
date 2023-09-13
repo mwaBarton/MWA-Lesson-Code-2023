7a27faee5a6e77c5d40f3a024e70b3af70cdac0e
mwaBarton <mwa@barton.ac.uk>
2023-09-13 16:02:20 +0100

    L101

---
+++ MWA 1F/L101 - If/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L101___If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.Write("Enter your name: ");
+
+            name = Console.ReadLine();
+
+            if (name == "Matt" || name == "Bob")
+            {
+                Console.WriteLine($"Hello {name}");
+            } 
+            else
+            {
+                Console.WriteLine("We don't accept that name!!!!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
