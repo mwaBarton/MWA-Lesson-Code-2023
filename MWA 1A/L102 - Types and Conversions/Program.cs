3ab70c5dee5d6860ce820aa807aba417344751d3
mwaBarton <mwa@barton.ac.uk>
2023-09-14 16:01:14 +0100

    L102

---
+++ MWA 1A/L102 - Types and Conversions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L102___Types_and_Conversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            int myNum = 5;
+
+            string myNumString = myNum.ToString();
+
+            Console.ReadKey();
+        }
+    }
+}
