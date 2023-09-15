1fb01d9cbb433cac76ce8a7d3388a1079ddb3c35
mwaBarton <mwa@barton.ac.uk>
2023-09-15 11:54:42 +0100

    L102

---
+++ MWA 1B/L102 - Types and Conversions/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L102___Types_and_Conversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+
+            double d = num * 2;
+
+            // Banker's rounding
+            int num2 = (int) Math.Round(num * 2.1);
+
+            Console.WriteLine(num2);
+
+            Console.WriteLine((int) (12.5 + 0.5));
+
+
+            // Let's talk about strings...
+
+            string name = "Matt";
+
+            char letter = name[0];
+
+            int x = letter;
+
+            Console.WriteLine(((char) (int) Math.Round(x+1.5)).ToString());
+
+            bool b = bool.Parse(Console.ReadLine());
+            Console.WriteLine(b);
+
+            b = true;
+            b = false;
+
+            if (name == "Matt")
+            {
+                b = true;
+            } else
+            {
+                b = false;
+            }
+
+            b = (name == "Matt");
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+        }
+    }
+}
