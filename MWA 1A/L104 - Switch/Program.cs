a53e5af5ce91114a81787b677766bad5a3688f83
mwaBarton <mwa@barton.ac.uk>
2023-09-20 11:51:39 +0100

    L104

---
+++ MWA 1A/L104 - Switch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L104___Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numBags;
+
+            Console.Write("How many bags did you use: ");
+            numBags = Console.ReadLine();
+
+            switch (numBags)
+            {
+                case "None":
+                    Console.WriteLine("You have used no bags. Well done");
+                    break;
+                default:
+                    int bags = int.Parse(numBags);
+
+                    switch (bags)
+                    {
+                        case 1:
+                            Console.WriteLine("That's very efficient!");
+                            break;
+                        case 2:
+                            Console.WriteLine("That's fine. This is a par 2 shopping centre.");
+                            break;
+                        case 3:
+                            Console.WriteLine("You're using too many bags");
+                            break;
+                        default:
+                            Console.WriteLine("WHAaaAATTTTTA!!!!!1");
+                            break;
+                    }
+
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
