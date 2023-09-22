261c065e67f5ab745486cb5ec0e12fa20f7ebd05
mwaBarton <mwa@barton.ac.uk>
2023-09-22 12:00:51 +0100

    L105

---
+++ MWA 1B/L105 - For/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L105___For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    s += "*";
+                }
+                s += "\n";
+            }
+
+            Console.WriteLine(s);
+
+            Console.ReadKey();
+        }
+    }
+}
