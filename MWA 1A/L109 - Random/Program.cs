1caa361b3bab8621b829f4bf2b543ebc69735516
mwaBarton <mwa@barton.ac.uk>
2023-10-02 14:15:09 +0100

    L109

---
+++ MWA 1A/L109 - Random/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace L109___Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random prng = new Random();
+
+            if (prng.NextDouble() < 0.45)
+            {
+                Console.WriteLine("It happened");
+            }
+
+            while (true)
+            {
+                int num = prng.Next(1, 7); // 1 <= num < 7
+                Console.WriteLine(num);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
