18c5c793bfcb33d3ad8678cd4b7e000ee1ea0d45
mwaBarton <mwa@barton.ac.uk>
2023-10-10 14:14:07 +0100

    L111

---
+++ MWA 1F/L111 - Subroutines/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L111___Subroutines
+{
+    internal class Program
+    {
+        static string name; // A global variable
+
+        static void SayGoodBye()
+        {
+            string x = "Hello"; // A local variable
+            Console.WriteLine($"gOODBYE {name}");
+        }
+
+        static void SayHello()
+        {
+            Console.WriteLine("What's your name?");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+            SayGoodBye();
+        }
+
+        static void Main(string[] args)
+        {
+            SayHello();
+
+            Console.WriteLine("does soemthing");
+
+            SayHello();
+
+            Console.ReadKey();
+        }
+    }
+}
