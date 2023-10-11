e4c50b7b3e7cb764cc144d973d4ff94bb972d68b
mwaBarton <mwa@barton.ac.uk>
2023-10-11 15:42:52 +0100

    L112

--- MWA 1F/L111 - Subroutines/Program.cs
+++ MWA 1F/L111 - Subroutines/Program.cs
@@ -26,13 +26,42 @@ namespace L111___Subroutines
 
         static void Main(string[] args)
         {
+            string myColour = "pink";
+            SetConsoleColour(myColour, true);
+
             SayHello();
 
             Console.WriteLine("does soemthing");
 
+            SetConsoleColour("green", false);
+
             SayHello();
 
             Console.ReadKey();
+        }
+
+        static void SetConsoleColour(string colour, bool message)
+        {
+            switch (colour.ToLower())
+            {
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case "magenta":
+                case "pink":
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+            }
+
+            if (message)
+            {
+                Console.WriteLine($"Console colour was updated to {colour}");
+            }
+
+            //Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), colour);
         }
     }
 }
