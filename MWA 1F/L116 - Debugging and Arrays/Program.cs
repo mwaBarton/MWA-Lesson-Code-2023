90f2e4988c4a31f1f0c22a013978072fd52d4be1
mwaBarton <mwa@barton.ac.uk>
2023-10-20 10:15:48 +0100

    L116

--- MWA 1F/L116 - Debugging and Arrays/Program.cs
+++ MWA 1F/L116 - Debugging and Arrays/Program.cs
@@ -8,37 +8,48 @@ namespace L116___Debugging_and_Arrays
 {
     internal class Program
     {
+
         static void WhatCouldPossiblyGoWrong(int x)
         {
             WhatCouldPossiblyGoWrong(x - 1);
         }
 
-
         static void Main(string[] args)
         {
-            int[] myArray;
-            myArray = new int[7];
 
-            // One index at a time
+            //WhatCouldPossiblyGoWrong(10);
+
+            //int total = 0;
+            //for (int i = 0; i < 1000000; i++)
+            //{
+            //    total += (int)Math.Pow(i, 2);
+            //    Console.WriteLine($"i: {i}, adding: {(int)Math.Pow(i, 2)}, total: {total}");
+            //}
+            //Console.WriteLine(total);
+
+
+            int num;
+            num = 5;
+
+
+            int[] myArray;
+            myArray = new int[6];
+
             myArray[0] = 1;
             myArray[1] = 1;
             myArray[2] = 2;
             myArray[3] = 3;
             myArray[4] = 5;
             myArray[5] = 8;
-            myArray[6] = 13;
 
-            // Using a for loop
             for (int i = 0; i < myArray.Length; i++)
             {
-                Console.Write($"Enter number {i}: ");
+                Console.Write($"Enter element {i}: ");
                 myArray[i] = int.Parse(Console.ReadLine());
             }
 
-            // auto-initialise a new array
-            string[] names = { "Matt", "Alex", "Kacper", "Numaya", "Amy" };
-
             Console.WriteLine(myArray[0]);
+
 
             Console.ReadKey();
         }
