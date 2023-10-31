5a91e8f5fb19283d2b52e55ab4118c3ae0ba7219
mwaBarton <mwa@barton.ac.uk>
2023-10-31 14:18:31 +0000

    L117

---
+++ MWA 1A/L117 - Lists/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L117___Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare list
+            List<int> numbers;
+
+            // Create list and associate it with my variable
+            numbers = new List<int>();
+
+            // Populate list
+            numbers.Add(5);
+            numbers.Add(-1);
+            numbers.Add(1024);
+            numbers.Add(20);
+
+            numbers.Remove(-1);
+
+            // Update elements
+            numbers[0] = 10;
+
+            // Print elements
+            Console.WriteLine(numbers[0] + " " + numbers[1]);
+
+            // Print length
+            Console.WriteLine(numbers.Count);
+
+            // Create new list
+            //numbers = new List<int>();
+            //Console.WriteLine(numbers.Count);
+
+            // Loop through the list
+            //for (int i = 0; i < numbers.Count; i++)
+            //{
+            //    Console.Write(numbers[i] + " ");
+            //}
+
+            foreach(int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1F/L117 - Lists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L117___Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] a = new int[10];
+
+            // Declaring a list variable, which is a reference to a list object
+            List<int> numbers;
+
+            // Create a new list and store the reference in numbers
+            numbers = new List<int>();
+
+            // Populate the list
+            numbers.Add(5);
+            numbers.Add(-10);
+            numbers.Add(500);
+            numbers.Remove(-10);
+            numbers.Add(1001);
+            numbers.RemoveAt(0);
+
+            // Get length of list
+            Console.WriteLine(numbers.Count);
+
+            // Access elements
+            numbers[0] = 50;
+            Console.WriteLine(numbers[0] + " " + numbers[1]);
+
+            numbers = new List<int>();
+            Console.WriteLine(numbers.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
