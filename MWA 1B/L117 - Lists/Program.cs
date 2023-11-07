4e353f16aaa0060eabc462f7063fb981ad839736
mwaBarton <mwa@barton.ac.uk>
2023-11-07 14:14:45 +0000

    L120

--- MWA 1B/L117 - Lists/Program.cs
+++ MWA 1B/L117 - Lists/Program.cs
@@ -10,6 +10,29 @@ namespace L117___Lists
     {
         static void Main(string[] args)
         {
+            // Declared a variable to store a reference to a list object
+            List<int> numbers;
+
+            // Creating a new list and storing its reference in the variable
+            numbers = new List<int>();
+
+            // Populate the list
+            numbers.Add(5);
+            numbers.Add(-10);
+            numbers.Add(500);
+            numbers.Add(1001);
+            numbers.Remove(-10);
+            numbers.RemoveAt(0);
+
+            // Access particular list elements
+            numbers[0] = 50;
+            Console.WriteLine(numbers[0] + " " + numbers[1]);
+
+            // Find the length of the list
+            Console.WriteLine(numbers.Count);
+
+            numbers = new List<int>();
+            Console.WriteLine(numbers.Count);
         }
     }
 }
---
+++ MWA 1F/L120 - Lists/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L120___Lists
+{
+    internal class Program
+    {
+        static void printList(List<string> list)
+        {
+            Console.Write("[");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i]);
+                if (i < list.Count - 1) Console.Write(", ");
+            }
+            Console.WriteLine("]");
+        }
+
+        static void SplitStringIntoList(string str)
+        {
+            printList(str.Split(' ').ToList());
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> shopping = new List<string>() { "Eggs", "Milk", "Flour" };
+            printList(shopping);
+
+            // 4. Split string into list
+            Console.Write("Enter a string with spaces in: ");
+            SplitStringIntoList(Console.ReadLine());
+
+            // 3. Remove duplicates
+            List<string> names = new List<string>();
+
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                Console.Write("Enter a name, or 'done' to stop: ");
+                string name = Console.ReadLine();
+
+                if (name == "done") keepGoing = false;
+                else if (!names.Contains(name)) names.Add(name);
+            }
+
+            Console.WriteLine($"There are {names.Count} unique names.");
+            printList(names);
+
+
+            Console.ReadKey();
+        }
+    }
+}
