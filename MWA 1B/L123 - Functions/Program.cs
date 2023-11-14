0578aa744fe55fc3f48112f546f7030dddef6bf5
mwaBarton <mwa@barton.ac.uk>
2023-11-14 16:00:47 +0000

    L123

---
+++ MWA 1B/L123 - Functions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L123___Functions
+{
+    internal class Program
+    {
+        static string SayAah(int angerLevel)
+        {
+            string result = "A";
+            for (int i = 0; i < angerLevel; i++)
+            {
+                result += "a";
+            }
+            result += "h";
+
+            if (angerLevel > 5)
+            {
+                result += "!";
+            }
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string myString = SayAah(5);
+
+            Console.WriteLine(myString);
+            Console.ReadKey();
+        }
+    }
+}
