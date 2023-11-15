18252d6839423ba7c02c0982660deb87c949c2b4
mwaBarton <mwa@barton.ac.uk>
2023-11-15 15:59:43 +0000

    L123

---
+++ MWA 1F/L123 - Functions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L123___Functions
+{
+    internal class Program
+    {
+        static float Add3POINtFive(float number)
+        {
+            Console.WriteLine("Boo");
+            return number + 3.5f;
+            Console.WriteLine("Hi");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add3POINtFive(10f));
+            Console.ReadKey();
+        }
+    }
+}
