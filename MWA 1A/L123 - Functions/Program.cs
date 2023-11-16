81aedde98a4cf8a562c46b70dd31d5be73fae472
mwaBarton <mwa@barton.ac.uk>
2023-11-16 15:57:10 +0000

    L123

---
+++ MWA 1A/L123 - Functions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L123___Functions
+{
+    internal class Program
+    {
+        static string PizzaRandomiser(List<string> toppings)
+        {
+            Console.WriteLine("boo");
+
+            switch (toppings.Count)
+            {
+                case 0:
+                    return "the null shape";
+                case 1:
+                    return "circle";
+                case 2:
+                    return "square";
+                default:
+                    return "triangle";
+            }
+
+            
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> myToppings = new List<string>() { "mozzarella", "anchovies", "pineapple" };
+
+            Console.WriteLine(PizzaRandomiser(myToppings));
+            Console.ReadKey();
+        }
+    }
+}
