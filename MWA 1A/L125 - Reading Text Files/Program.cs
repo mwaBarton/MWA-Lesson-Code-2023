532e1129bb90880961bd41a3a5761ef14d9df1a6
mwaBarton <mwa@barton.ac.uk>
2023-11-22 11:59:35 +0000

    L125

---
+++ MWA 1A/L125 - Reading Text Files/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace L125___Reading_Text_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "menu.txt";
+
+            // The easy way
+            //string contents = File.ReadAllText(filename);
+
+            //Console.WriteLine(contents);
+
+
+
+            // The proper way
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1A/L125 - Structures/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L125___Structures
+{
+    internal class Program
+    {
+        struct MenuItem
+        {
+            public string name;
+            public double price;
+            public bool isSpicy;
+        }
+
+        static void printMenu(MenuItem[] menu)
+        {
+            foreach (MenuItem item in menu)
+            {
+                Console.WriteLine($"{item.name} costs £{item.price}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            MenuItem item1;
+            item1.name = "kebab";
+            item1.price = 7.99;
+            item1.isSpicy = true;
+
+            MenuItem[] menu = new MenuItem[3];
+            menu[0] = item1;
+            menu[1].name = "Single piece of chicken";
+            menu[1].price = 60.99;
+            menu[1].isSpicy = false;
+            menu[2].name = "banana";
+            menu[2].price = 0.106;
+            menu[2].isSpicy = true;
+
+            printMenu(menu);
+
+            Console.ReadKey();
+        }
+    }
+}
