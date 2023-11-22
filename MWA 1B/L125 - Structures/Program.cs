bc371d6cfbe870dd28b8a7ee79e5d029ee8f78b2
mwaBarton <mwa@barton.ac.uk>
2023-11-22 16:02:44 +0000

    L125

---
+++ MWA 1B/L125 - Structures/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L125___Structures
+{
+    internal class Program
+    {
+        struct Animal
+        {
+            public double volume;
+            public string generalColourSortOf;
+            public double speed;
+        }
+
+        static void PrintZoo(Animal[] animals)
+        {
+            foreach (Animal animal in animals)
+            {
+                Console.WriteLine($"I have a {animal.generalColourSortOf} animal. " +
+                    $"It is {animal.volume}m^3 and moves at {animal.speed}ms^-1");
+            }
+        }
+
+        static void PrintAnimal(Animal a)
+        {
+
+        }
+
+
+        static void PrintInt(int x)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Animal a1;
+            a1.volume = 27.6;
+            a1.generalColourSortOf = "Grey";
+            a1.speed = 100;
+
+            Animal a2 = new Animal();
+
+            PrintAnimal(a2);
+
+            Animal[] zoo = new Animal[3];
+            zoo[0] = a1;
+            zoo[1].volume = 4;
+            zoo[1].generalColourSortOf = "White";
+            zoo[1].speed = -5.6;
+            zoo[2].volume = 5000;
+            zoo[2].generalColourSortOf = "Rainbow";
+            zoo[2].speed = 0.0005;
+
+            PrintZoo(zoo);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1F/L125 - Structures/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L125___Structures
+{
+    internal class Program
+    {
+        struct Pencilcase
+        {
+            public int numberOfPencils;
+            public int numberOfPens;
+            public bool isThereACompassInThere;
+            public List<string> penColours;
+            public string name;
+        }
+
+        static void DoSoemthing(Pencilcase p)
+        {
+
+        }
+
+        static void PrintPencilcases(Pencilcase[] cases)
+        {
+            foreach(Pencilcase p in cases) {
+                Console.WriteLine($"{p.name} is a pencilcase " +
+                    $"with {p.numberOfPens} pens of {p.penColours.Count} colours.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Pencilcase jeremy = new Pencilcase();
+
+            jeremy.name = "Jeremy";
+            jeremy.numberOfPens = 4;
+            jeremy.numberOfPencils = 1;
+            jeremy.penColours = new List<string>() { "Black", "Red" };
+
+            DoSoemthing(jeremy);
+
+            Pencilcase[] pencilcases = new Pencilcase[2];
+            pencilcases[0] = jeremy;
+            pencilcases[1].name = "Jonathon";
+            pencilcases[1].isThereACompassInThere=true;
+            pencilcases[1].numberOfPencils = 2;
+            pencilcases[1].numberOfPens = 11;
+            pencilcases[1].penColours = new List<string>() { "Green", "Yellow", "Black", "Pink", "Blue", "Red" };
+
+            PrintPencilcases(pencilcases);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
