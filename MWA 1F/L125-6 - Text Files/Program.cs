891070c9e67bec02204fbc4837f2eef159441146
mwaBarton <mwa@barton.ac.uk>
2023-11-24 10:10:53 +0000

    L125

---
+++ MWA 1F/L125-6 - Text Files/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace L125_6___Text_Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "shopping.txt";
+
+            // Easy way (writing)
+            File.WriteAllText(filename, "yeast\nmilk\neggs\n");
+
+            // East way (reading)
+            string contents;
+
+            contents = File.ReadAllText(filename);
+
+            Console.WriteLine($"Contents of {filename} are \"{contents.Replace("\n", ", ")}\"");
+
+
+            // Proper way (write)
+            using (StreamWriter writer = new StreamWriter(filename, false))
+            {
+                writer.WriteLine("yeast");
+            }
+
+            // Proper way (reading)
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                string line;
+
+                while (reader.EndOfStream == false)
+                {
+                    line = reader.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
