529991e2c03978f99cde8a0d9f0f2fd4810049c0
mwaBarton <mwa@barton.ac.uk>
2024-02-07 11:58:58 +0000

    L148

---
+++ MWA 1A/L148 - Classes and Objects/Drink.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L148___Classes_and_Objects
+{
+    internal class Drink
+    {
+        public string type;
+        public bool isAlcoholic;
+        public double viscosity;
+
+        public void DrinkIt()
+        {
+            if (isAlcoholic)
+            {
+                Console.WriteLine("You got drunk. Go home (in a taxi).");
+            } 
+            else if (viscosity > 100) 
+            {
+                Console.WriteLine("Bit gloopy...");
+            }
+            else {
+                Console.WriteLine("You drank the drink. Well done you.");
+            }
+        }
+    }
+}
---
+++ MWA 1A/L148 - Classes and Objects/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L148___Classes_and_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Drink myDrink = new Drink();
+
+            myDrink.viscosity = 50;
+            myDrink.isAlcoholic = true;
+            myDrink.type = "Wkd";
+
+            myDrink.DrinkIt();
+
+            Console.ReadKey();
+        }
+    }
+}
