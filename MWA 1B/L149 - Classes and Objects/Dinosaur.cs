a114f028fccd089f008da8b9df0d2c04ccb7478e
mwaBarton <mwa@barton.ac.uk>
2024-02-07 14:14:36 +0000

    L149

---
+++ MWA 1B/L149 - Classes and Objects/Dinosaur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L149___Classes_and_Objects
+{
+    internal class Dinosaur
+    {
+        public bool isAlive;
+        public double runningSpeed;
+        public double mass;
+        public string name;
+
+        public void Eat()
+        {
+            if (isAlive)
+            {
+                Console.Write("Yumm");
+                for (int i = 0; i < mass; i++)
+                {
+                    Console.Write("m");
+                }
+                Console.WriteLine("y");
+            } else
+            {
+                Console.WriteLine("Nothing happens...");
+            }
+        }
+    }
+}
---
+++ MWA 1B/L149 - Classes and Objects/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L149___Classes_and_Objects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dinosaur jimmy = new Dinosaur();
+
+            jimmy.isAlive = true;
+            jimmy.runningSpeed = 100;
+            jimmy.mass = 100;
+            jimmy.name = "Pterodactyl";
+
+            jimmy.Eat();
+
+            Console.ReadKey();
+
+            jimmy.isAlive = false;
+            jimmy.Eat();
+
+            Console.ReadKey();
+        }
+    }
+}
