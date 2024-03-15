25902ed63abad162ac5e348f2ce8c79fed31859a
mwaBarton <mwa@barton.ac.uk>
2024-03-15 11:52:54 +0000

    L155

---
+++ MWA 1B/L155 - Polymorphism/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L155___Polymorphism
+{
+
+    public class Animal
+    {
+        public  void Talk()
+        {
+            Console.WriteLine("*makes noise*");
+        }
+    }
+    public class Dinosaur : Animal
+    {
+        public  void Talk()
+        {
+            Console.WriteLine("rah");
+        }
+    }
+    public class Ferret : Animal
+    {
+        public  void Talk()
+        {
+            Console.WriteLine("shquheuahkh");
+        }
+    }
+
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            List<Animal> animals = new List<Animal>();
+            animals.Add(new Animal());
+            animals.Add(new Dinosaur());
+            animals.Add(new Ferret());
+
+            foreach (Animal animal in animals)
+            {
+                animal.Talk();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MWA 1F/L155 - Polymorphism/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L155___Polymorphism
+{
+    internal class Program
+    {
+        public class Hat
+        {
+            public int x = 6;
+
+            public virtual string SayHello()
+            {
+                return "Hello, I am a hat!";
+            }
+        }
+
+        public class BowlerHat : Hat
+        {
+            public int y = 3;
+
+            public override string SayHello()
+            {
+                return "Hello, I am a Bowler Hat!!";
+            }
+        }
+
+        public class HardHat : Hat
+        {
+            public override string SayHello()
+            {
+                string s = base.SayHello();
+                return s + "\n\nHello, it's me, the Hard Hat!!!!!";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Hat> hats = new List<Hat>();
+
+            hats.Add(new BowlerHat());
+            hats.Add(new BowlerHat());
+            hats.Add(new HardHat());
+            hats.Add(new Hat());
+
+            foreach(Hat hat in hats)
+            {
+                Console.WriteLine(hat.SayHello());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
