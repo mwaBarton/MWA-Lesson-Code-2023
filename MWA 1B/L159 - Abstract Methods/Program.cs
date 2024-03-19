171ecef2d4ae6da62b26e263a570902dfaa44f12
mwaBarton <mwa@barton.ac.uk>
2024-03-19 17:35:56 +0000

    L159

---
+++ MWA 1B/L159 - Abstract Methods/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace L159___Abstract_Methods
+{
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double inR)
+        {
+            radius = inR;
+        }
+
+        public override double GetArea()
+        {
+            return radius * radius * Math.PI;
+        }
+    }
+    public class Square : Shape
+    {
+        private double sideLength;
+
+        public Square(double side)
+        {
+            sideLength = side;
+        }
+
+        public override double GetArea()
+        {
+            return sideLength * sideLength;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape myShape = new Square(3);
+
+            Console.WriteLine(myShape.GetArea());
+
+            Console.ReadKey();
+        }
+    }
+}
