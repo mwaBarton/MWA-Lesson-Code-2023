180823026c94ffa778cccf9aa9d482b4d9a19720
mwaBarton <mwa@barton.ac.uk>
2024-04-15 14:14:45 +0100

    L164

--- MWA 1B/L159 - Abstract Methods/Program.cs
+++ MWA 1B/L159 - Abstract Methods/Program.cs
@@ -6,6 +6,50 @@ namespace L159___Abstract_Methods
     {
         public abstract double GetArea();
     }
+
+    public abstract class Shape3D : Shape
+    {
+        public abstract double GetVolume();
+    }
+
+    public class Sphere : Shape3D
+    {
+        private double radius;
+
+        public Sphere(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return 4 * Math.PI * Math.Pow(radius, 2);
+        }
+
+        public override double GetVolume()
+        {
+            return 4 / 3 * Math.PI * Math.Pow(radius, 3);
+        }
+    }
+
+
+    public class Trapezium : Shape
+    {
+        private double a, b, h;
+
+        public Trapezium(double a, double b, double h)
+        {
+            this.a = a;
+            this.b = b;
+            this.h = h;
+        }
+
+        public override double GetArea()
+        {
+            return 0.5 * (a + b) * h;
+        }
+    }
+
     public class Circle : Shape
     {
         private double radius;
