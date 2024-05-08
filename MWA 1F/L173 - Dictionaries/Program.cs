4776ae8a6669dfd75f1849528670bebbf7c6455a
mwaBarton <mwa@barton.ac.uk>
2024-05-08 15:58:49 +0100

    L173

---
+++ MWA 1F/L173 - Dictionaries/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace L173___Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> faveNums = new Dictionary<string, int>();
+
+            faveNums.Add("Oscar", 3);
+            faveNums.Add("Areeb", 7);
+            faveNums.Add("Quinn", 9);
+            faveNums.Add("Dan", 6);
+
+            Console.WriteLine(faveNums.Count);
+
+            faveNums.Remove("Dan");
+
+            faveNums["Areeb"]++;
+
+            Console.WriteLine(faveNums["Areeb"]);
+
+            foreach (string name in faveNums.Keys)
+            {
+                Console.WriteLine($"{name}'s favourite number is {faveNums[name]}");
+            }
+
+            Task1();
+
+            Console.ReadKey();
+        }
+
+        static void Task1()
+        {
+            Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            foreach (string word in input.Split(' '))
+            {
+                if (counts.ContainsKey(word))
+                {
+                    counts[word]++;
+                }
+                else
+                {
+                    counts.Add(word, 1);
+                }
+            }
+
+            foreach (string word in counts.Keys)
+            {
+                Console.WriteLine($"{word} : {counts[word]}");
+            }
+        }
+    }
+}
---
+++ MWA 1F/L173 - Dictionaries/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("L173 - Dictionaries")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Barton Peveril IT Services")]
+[assembly: AssemblyProduct("L173 - Dictionaries")]
+[assembly: AssemblyCopyright("Copyright © Barton Peveril IT Services 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f58aaef6-0a8f-4df7-8503-aeb8a9477b5a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
