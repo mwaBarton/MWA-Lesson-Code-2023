382552054c488e1c569ebe16ac45e26b7b975fd6
mwaBarton <mwa@barton.ac.uk>
2024-07-03 12:46:47 +0100

    L185

---
+++ MWA 1A/L185 - Exceptions 2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace L185___Exceptions_2
+{
+    public class UnexpectedFourException : Exception
+    {
+        public string offendingUsername;
+
+        public UnexpectedFourException(string offendingUsername) : base($"Oh dear, another four appeared!!! It was {offendingUsername}'s fault")
+        {
+            this.offendingUsername = offendingUsername;
+        }
+    }
+
+    internal class Program
+    {
+        static int GetNumFromUser(string user)
+        {
+            Console.Write("Enter a number other than 4: ");
+            int result = int.Parse(Console.ReadLine());
+
+            //if (result == 4)
+            //{
+            //    throw new UnexpectedFourException(user);
+            //}
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string username;
+
+            Console.WriteLine("Hello, what is your name? ");
+            username = Console.ReadLine();
+
+            while (true)
+            {
+                try
+                {
+                    int x = GetNumFromUser(username);
+
+                    if (x == 4)
+                    {
+                        throw new UnexpectedFourException(username);
+                    }
+                }
+                catch (UnexpectedFourException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\n" + e.Message + "\n" + e.offendingUsername + " didn't follow the rules!\n");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                catch (FormatException e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("\nYou must enter a number. Try again!\n");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+        }
+    }
+}
